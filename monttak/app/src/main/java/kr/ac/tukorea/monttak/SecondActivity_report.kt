5f36f6056e3e33fba7f6e921f0f843f46a3af273
Sammuelwoojae <seoullian@gmail.com>
2022-07-30 23:02:40 +0900

    initial xml commit

---
+++ monttak/app/src/main/java/kr/ac/tukorea/monttak/SecondActivity_report.kt
@@ -0,0 +1,11 @@
+package kr.ac.tukorea.monttak
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity_report : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second_report)
+    }
+}
